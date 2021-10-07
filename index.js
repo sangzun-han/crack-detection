0a5cea8a3c5901752203d440996a30800ab4839f
sangzun-han <hsjun1996@naver.com>
2021-10-07 22:14:32 +0900

    Update : config

---
+++ index.js
@@ -0,0 +1,29 @@
+function handleImageView(files) {
+  let file = files[0];
+
+  if (!file.type.match(/image.*/)) {
+    alert('not image file!');
+  }
+
+  let reader = new FileReader();
+  reader.onload = function (e) {
+    let img = new Image();
+    img.onload = function () {
+      let ctx = document.getElementById('canvas').getContext('2d');
+      ctx.drawImage(img, 0, 0, 480, 320);
+    };
+    img.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+}
+
+function clickEvent(event) {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  context.beginPath();
+  context.arc(event.layerX, event.layerY, 3, 0, Math.PI * 2);
+  context.fill();
+  console.log(event.layerX, event.layerY);
+}
+canvas.addEventListener('click', clickEvent);
